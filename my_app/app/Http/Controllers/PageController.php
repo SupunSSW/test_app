141e6a3826a74f959625399f585e2846ce4adbdb
SupunSSW <ssadeepa7@gmail.com>
2020-05-27 14:49:07 +0530

    added user view function

--- my_app/app/Http/Controllers/PageController.php
+++ my_app/app/Http/Controllers/PageController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Users;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index() {
-        return view('index');
+        $users = Users::all();
+        return view('index')->with('users', $users);
     }
 
     public function add() {
--- my_app/resources/views/index.blade.php
+++ my_app/resources/views/index.blade.php
@@ -2,4 +2,21 @@
 
 @section('content')
     <h2>View Users</h2>
+
+    @foreach($users as $user)
+
+        <div class="text-white bg-dark mb-3 p-3">
+            <li>
+                {{$user->fname}}
+            </li>
+            <li>
+                {{$user->lname}}
+            </li>
+            <li>
+                {{$user->email}}
+            </li>
+        </div>
+        
+    @endforeach
+
 @endsection
